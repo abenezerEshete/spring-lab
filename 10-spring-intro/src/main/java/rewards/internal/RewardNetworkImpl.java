169653dc4ec700baa5dc4098cd5af61f75ade897
Abenezer <abenezereshete7@gmail.com>
2024-12-28 17:09:15 -0600

    Implement:  The RewardNetworkImpl Application Logic:    TODO-07: Write code here for rewarding an account according to the sequence diagram in the lab document    TODO-08: Return the corresponding reward confirmation

--- 10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ 10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,9 +1,13 @@
 package rewards.internal;
 
+import common.money.MonetaryAmount;
+import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.internal.account.Account;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
@@ -50,9 +54,18 @@ public class RewardNetworkImpl implements RewardNetwork {
 	}
 
 	public RewardConfirmation rewardAccountFor(Dining dining) {
+
+		Account account = accountRepository.findByCreditCard(dining.getCreditCardNumber());
+		Restaurant restaurant = restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
+		MonetaryAmount monetaryAmount = restaurant.calculateBenefitFor(account, dining);
+		AccountContribution contribution = account.makeContribution(monetaryAmount);
+
+		accountRepository.updateBeneficiaries(account);
+		RewardConfirmation confirmation = rewardRepository.confirmReward(contribution, dining);
+
 		// TODO-07: Write code here for rewarding an account according to
 		//          the sequence diagram in the lab document
 		// TODO-08: Return the corresponding reward confirmation
-		return null;
+		return confirmation;
 	}
 }
